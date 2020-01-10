cbea1ef42fe4e86352f4543c3757a483930de331
hota1024 <hotalog@hota1024.com>
2020-01-10 16:27:01 +0900

    :white_check_mark: Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,12 @@
-module.exports = {}
+module.exports = {
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json'
+    }
+  },
+  testMatch: ['**/tests/**/*.test.ts']
+}
